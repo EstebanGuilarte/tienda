bb5ccab572f878ec731f3a8096b25fdd17b085b4
EstebanGuilarte <eaguilartes0802@gmail.com>
2022-10-13 15:39:29 -0600

    Semana 4, uso de MySQL inicial

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,34 @@
+package com.tienda.controller;
+
+import com.tienda.dao.ClienteDao;
+import com.tienda.domain.Cliente;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    
+    @Autowired //Para crear el objeto o para que se use si ya existe **No hace más de un objeto
+    private ClienteDao clienteDao;
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        var texto="Estamos en semana #4";
+        model.addAttribute("saludo", texto);
+        
+        /*Cliente cliente1=new Cliente("Pedro","Jiménez Retana","pjimenez@gmail.com","8888-8888");
+        Cliente cliente2=new Cliente("Juan","Retana Jiménez","jretana@gmail.com","9999-9999");
+        Cliente cliente3=new Cliente("Rodrigo","Retana Jiménez","Rretana@gmail.com","9999-9999");
+        
+        var clientes=Arrays.asList(cliente1,cliente2,cliente3);*/
+        
+        var clientes = clienteDao.findAll();
+        
+        model.addAttribute("clientes", clientes);
+        
+        return "index";
+    }
+}
---
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -0,0 +1,32 @@
+package com.tienda.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="cliente")
+public class Cliente implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id //Marca al siguiente atributo com la llame primaria (En este caso idCliente)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCliente;//id_Cliente en base de datos --> idCliente en java (Siempre se aplica éste tipo de cambios)
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String telefono;
+
+    public Cliente(String nombre, String apellidos, String correo, String telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Cliente() {
+    }
+    
+}
